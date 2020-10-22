523b7d71345ae1ec0c97265c803e929707ffd680
mega2799 <megadigimatt@gmail.com>
2020-10-22 11:41:28 +0200

    to finish ridirect

--- theory/read.sh
+++ theory/read.sh
@@ -38,6 +38,24 @@ while read varA varB varC;
 exec 103 < readed.txt # num di file descriptor a cui fare riferimento
 read -N 4 -u 103 RIGA
 echo $RIGA # prim
-exec 103 < &- # chiudo tutto
+exec 103 <&- # chiudo tutto
 
+# Rindirizzzamento
+# Una subshell ha una copia dei file descriptor del padre che però puo decidere di modificare
+# cosa può vedere il figlio, cambiando ad esempio lo stream
+# Un processo puo in alterntiva auto ridirezionarsi avendo l' impressione di scrivere su un
+# certo output mentre in realtà sta usando un altro file descriptor
+
+# Operatori di ridirezionamento
+# < (ricevere input da file ) > (mandare stdout verso file eliminandone il precedente contenuto)
+# >> (mandare stdout verso file aggiungendosi al precedente contenuto) | (ridirige output di un altro programma)
+
+# uso in contemporanea
+# program < file_input > file_output
+
+# ridireziono stderror e stdoutput
+# program &> nome_file_error_and_output
+
+# come prima ma in due file diversi
+# program #> nome_file_error > nome_file_output (# numero file descriptor)
 
