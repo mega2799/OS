0c01e92f837ae421dcf230788ffae1febbd7e1ce
mega2799 <megadigimatt@gmail.com>
2020-11-30 16:09:25 +0100

    trashed duplicated

--- eser/makefiles/1.quadratoDimezza/separate/dimezza.h
+++
@@ -1,5 +0,0 @@
-#ifndef __DIMEZZA_H__
-
-#define __DIMEZZA_H__
-extern double func(double arg);
-#endif 
--- eser/makefiles/2.AllocaVettore/macro.h
+++
@@ -1,5 +0,0 @@
-#define NUM_BYTES 40
-#define alloc(x) \
-	x=(uint*)malloc(NUM_BYTES); \
-	if( x != NULL){ for(int i=0;i<10;i++) \
-		{ x[i]= i - 1000;}} 
